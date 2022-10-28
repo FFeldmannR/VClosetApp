1309a3e3efd3743fde821e9314316e355541adb1
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-10-28 18:36:22 -0300

    chamada da classe msg em CadastroActivity

--- app/src/main/java/com/feldmann/vcloset/view/CadastroActivity.java
+++ app/src/main/java/com/feldmann/vcloset/view/CadastroActivity.java
@@ -3,12 +3,18 @@ package com.feldmann.vcloset.view;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.feldmann.vcloset.R;
+import com.feldmann.vcloset.controller.Controller;
+import com.feldmann.vcloset.controller.ViewController;
 
 public class CadastroActivity extends AppCompatActivity {
+    private final String tagLog = this.getClass().getName().toString();
+    private Controller.msg msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        this.msg = new ViewController(this, tagLog);
+        msg.logD("onCreate");
     }
 }
