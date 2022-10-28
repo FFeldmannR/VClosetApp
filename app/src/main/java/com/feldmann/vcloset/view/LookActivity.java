3a309d25fa13cbea1ff1034273efa68655d0109e
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-10-28 18:53:12 -0300

    criado classe LookActivity

---
+++ app/src/main/java/com/feldmann/vcloset/view/LookActivity.java
@@ -0,0 +1,14 @@
+package com.feldmann.vcloset.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.feldmann.vcloset.R;
+
+public class LookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_look);
+    }
+}
