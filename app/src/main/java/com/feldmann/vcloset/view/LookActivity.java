23533b35b20eb979a0a87ab4dc46c1b997d1f726
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-10-28 18:54:22 -0300

    feito a chamada de msg em LookActivity

--- app/src/main/java/com/feldmann/vcloset/view/LookActivity.java
+++ app/src/main/java/com/feldmann/vcloset/view/LookActivity.java
@@ -3,8 +3,11 @@ package com.feldmann.vcloset.view;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.feldmann.vcloset.R;
+import com.feldmann.vcloset.controller.Controller;
 
 public class LookActivity extends AppCompatActivity {
+    private final String tagLog = this.getClass().getName().toString();
+    private Controller.msg msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
